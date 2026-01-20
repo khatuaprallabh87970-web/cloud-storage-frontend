fa70b55b7644082b4e7303831abc9bab609c0b47
Prallabh Prasad Khatua <khatuaprallabh87970@gmail.com>
2026-01-21 01:30:34 +0530

    Use env variable for API base URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cloud-storage-backend-843a.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
